c1f10cef61973fc229385dab86300a9842c02063
Rene Casula <rcasularodriguez@gmail.com>
2020-12-26 04:49:23 -0600

    Actualización final antes de subirlo a Github

--- cajero/cajero.js
+++ cajero/cajero.js
@@ -1,21 +1,35 @@
 class Billete
 {
-  constructor(v, c) {
+  constructor(n, v, c) {
+    this.imagen = new Image();
+    this.nombre = n;
     this.valor = v;
     this.cantidad = c;
+
+    this.imagen.src = imagenes[this.nombre]
   }
 }
 
+var imagenes = [];
+imagenes["500"] = "quinientosx.png";
+imagenes["100"] = "cienx.png";
+imagenes["50"] = "cincuentax.png";
+imagenes["20"] = "veintex.png";
+imagenes["10"] = "diezx.png";
+imagenes["5"] = "cincox.png";
+imagenes["2"] = "dosx.png";
+imagenes["1"] = "unox.png";
+
 var caja = [];
 var entregado = [];
-caja.push(new Billete(500,10));
-caja.push(new Billete(100,5));
-caja.push(new Billete(50,10));
-caja.push(new Billete(20,5));
-caja.push(new Billete(10,10));
-caja.push(new Billete(5,5));
-caja.push(new Billete(2,5));
-caja.push(new Billete(1,5));
+caja.push(new Billete("500",500,10));
+caja.push(new Billete("100",100,5));
+caja.push(new Billete("50",50,10));
+caja.push(new Billete("20",20,5));
+caja.push(new Billete("10",10,10));
+caja.push(new Billete("5",5,5));
+caja.push(new Billete("2",2,5));
+caja.push(new Billete("1",1,5));
 var dinero = 0;
 var div = 0;
 var papeles = 0;
@@ -38,10 +52,10 @@ function entregarDinero() {
           papeles = div;
       }
       if (papeles != 0) {
-        entregado.push(new Billete(bi.valor,papeles));
+        var bill = bi.valor.toString();
+        entregado.push(new Billete(bill,bi.valor,papeles));
         dinero -= (bi.valor * papeles);
         bi.cantidad -= papeles;
-        console.log(caja);
       }
     }
   }
@@ -50,7 +64,17 @@ function entregarDinero() {
     resultado.innerHTML = " No hay dinero suficiente ";
   } else {
       for(var e of entregado) {
-        resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br/>";
+        // resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br/>";
+        console.log(entregado);
+        var i = 0;
+        while ( i < e.cantidad ) {
+          console.log(e.imagen);
+          document.body.appendChild(e.imagen);
+          console.log(i);
+          console.log(e.cantidad);
+          console.log(" ");
+          i++;
+        }
       }
   }
-}
+}   // Fin de función entregarDinero
